c6d399172a45c8864fa2c7c690194eec911c7b69
Sadjow <sadjow@gmail.com>
2013-02-06 16:00:29 -0300

    Preparing for 0.22.0 release

--- mix-rails-auth/lib/mix-rails-auth/version.rb
+++ mix-rails-auth/lib/mix-rails-auth/version.rb
@@ -1,7 +1,7 @@
 module MixRailsAuth
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 21
+    MINOR = 22
     TINY  = 0
     PRE   = nil
 
--- mix-rails-settings/lib/mix-rails-settings/version.rb
+++ mix-rails-settings/lib/mix-rails-settings/version.rb
@@ -1,7 +1,7 @@
 module MixRailsSettings
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 21
+    MINOR = 22
     TINY  = 0
     PRE   = nil
 
