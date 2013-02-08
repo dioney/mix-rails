442293615fd9add940eb033c117b4745c5ca53c6
Sadjow <sadjow@gmail.com>
2013-02-08 12:08:33 -0300

    0.23.0 released with ActiveRecord

--- mix-rails-auth/lib/mix-rails-auth/version.rb
+++ mix-rails-auth/lib/mix-rails-auth/version.rb
@@ -1,7 +1,7 @@
 module MixRailsAuth
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 22
+    MINOR = 23
     TINY  = 0
     PRE   = nil
 
--- mix-rails-message-board/lib/mix-rails-message-board/version.rb
+++ mix-rails-message-board/lib/mix-rails-message-board/version.rb
@@ -1,7 +1,7 @@
 module MixRailsMessageBoard
   module VERSION #:nodoc:
     MAJOR = 0
-    MINOR = 22
+    MINOR = 23
     TINY  = 0
     PRE   = nil
 
